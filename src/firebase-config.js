e60da8fa60f27eb4ac4c40fefb94b4808f3c5969
Siddharth <siddharthsayan007@gmail.com>
2023-04-27 23:07:48 +0530

    updated firebase

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,13 +1,15 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_FIREBASE_CONFIG_API_KEY,
-    authDomain: process.env.REACT_APP_FIREBASE_CONFIG_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_FIREBASE_CONFIG_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_FIREBASE_CONFIG_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_FIREBASE_CONFIG_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_FIREBASE_CONFIG_APP_ID,
-    measurementId: process.env.REACT_APP_FIREBASE_CONFIG_MEASUREMENT_ID
+  
+    apiKey: "AIzaSyAQnrfpSmIDb95CcSvfNi1mB4oT9v-Z-PE",
+    authDomain: "food2-5377e.firebaseapp.com",
+    projectId: "food2-5377e",
+    storageBucket: "food2-5377e.appspot.com",
+    messagingSenderId: "180240099824",
+    appId: "1:180240099824:web:f092ad7df80a32b0b7d535",
+    measurementId: "G-E33XLB87TW"
+
 };
 
 export const app = initializeApp(firebaseConfig);
